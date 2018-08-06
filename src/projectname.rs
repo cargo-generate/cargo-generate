e75c6b586f55a6a425707d922cd9152484c212d9
k0pernicus <antonin.carette@gmail.com>
2018-08-06 23:34:23 +0200

    Add as attributes the kebab case + snake case strings

--- src/projectname.rs
+++ src/projectname.rs
@@ -3,13 +3,17 @@ use heck::{KebabCase, SnakeCase};
 /// Stores user inputted name and provides convenience methods
 /// for handling casing.
 pub struct ProjectName {
-    user_input: String,
+    pub user_input: String,
+    pub kebab_case_name: String,
+    pub snake_case_name: String,
 }
 
 impl ProjectName {
     pub fn new(name: &str) -> ProjectName {
         ProjectName {
             user_input: name.to_string(),
+            kebab_case_name: name.to_kebab_case(),
+            snake_case_name: name.to_snake_case(),
         }
     }
 
