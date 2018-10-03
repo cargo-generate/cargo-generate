3921b12d6b467a1bc3b2cbcf466b7d366801ef9b
k0pernicus <antonin.carette@gmail.com>
2018-10-03 20:44:12 +0200

    Implements a function to init and load git submodules from a given git repository

--- src/git.rs
+++ src/git.rs
@@ -1,5 +1,5 @@
 use git2::{
-    build::CheckoutBuilder, build::RepoBuilder, Repository as GitRepository, RepositoryInitOptions,
+    build::CheckoutBuilder, build::RepoBuilder, Repository as GitRepository, RepositoryInitOptions, SubmoduleUpdateOptions
 };
 use quicli::prelude::*;
 use remove_dir_all::remove_dir_all;
@@ -15,6 +15,24 @@ pub fn create(project_dir: &PathBuf, args: Args) -> Result<GitRepository> {
     }
 
     Ok(rb.clone(&args.git, &project_dir)?)
+}
+
+pub fn load_submodules(git_repository: &GitRepository) -> Result<()> {
+    let submodules = git_repository.submodules()?;
+    if submodules.len() == 0 {
+        return Ok(())
+    }
+
+    // Init submodule options
+    let mut submodule_opts = SubmoduleUpdateOptions::new();
+    submodule_opts.allow_fetch(true).checkout(CheckoutBuilder::new());
+
+    // Init and load each submodule
+    for mut submodule in submodules {
+        submodule.update(true, Some(&mut submodule_opts))?;
+    };
+
+    Ok(())
 }
 
 pub fn remove_history(project_dir: &PathBuf) -> Result<()> {
