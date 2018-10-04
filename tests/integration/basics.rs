4dac8229ac3b8c548219a83f2a799cc1654aeeed
Ashley Williams <ashley666ashley@gmail.com>
2018-10-04 11:51:27 -0400

    test(template): test date substitution

--- tests/integration/basics.rs
+++ tests/integration/basics.rs
@@ -41,6 +41,39 @@ version = "0.1.0"
 }
 
 #[test]
+fn it_substitutes_date() {
+    let template = dir("template")
+        .file(
+            "Cargo.toml",
+            r#"[package]
+name = "{{project-name}}"
+description = "A wonderful project Copyright {{ "now" | date: "%Y" }}"
+version = "0.1.0"
+"#,
+        ).init_git()
+        .build();
+
+    let dir = dir("main").build();
+
+    Command::main_binary()
+        .unwrap()
+        .arg("generate")
+        .arg("--git")
+        .arg(template.path())
+        .arg("--name")
+        .arg("foobar-project")
+        .current_dir(&dir.path())
+        .assert()
+        .success()
+        .stdout(predicates::str::contains("Done!").from_utf8());
+
+    assert!(
+        dir.read("foobar-project/Cargo.toml")
+            .contains("Copyright 2018")
+    );
+}
+
+#[test]
 fn it_kebabcases_projectname_when_passed_to_flag() {
     let template = dir("template")
         .file(
