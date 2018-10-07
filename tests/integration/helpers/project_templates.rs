0787873663d09f36a65cd6e05a39a1f6966a25cd
k0pernicus <antonin.carette@gmail.com>
2018-10-07 20:37:44 +0200

    Add helper file that contains project templates builder

---
+++ tests/integration/helpers/project_templates.rs
@@ -0,0 +1,25 @@
+use helpers::project_builder::{dir, ProjectBuilder};
+
+pub fn cargo_template_with_copyright() -> ProjectBuilder {
+    dir("template")
+    .file(
+        "Cargo.toml",
+        r#"[package]
+name = "{{project-name}}"
+description = "A wonderful project Copyright {{ "2018-10-04 18:18:45 +0200" | date: "%Y" }}"
+version = "0.1.0"
+"#,
+    )
+}
+
+pub fn default_cargo_template() -> ProjectBuilder {
+    dir("template")
+    .file(
+        "Cargo.toml",
+        r#"[package]
+name = "{{project-name}}"
+description = "A wonderful project"
+version = "0.1.0"
+"#,
+    )
+}
