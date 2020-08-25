8db4ec111a8e0fdb9ec4f8b74db1a8112afc9c3d
Rebecca Turner <rbt@sent.as>
2020-08-25 15:07:48 -0400

    Support abbreviated owner/repo Git URL format
    The GitHub [hub] tool allows using `owner/repo` as an abbreviated form
    of `https://github.com/owner/repo.git`. This change supports that format
    with the `--git` command-line argument.
    
    It also improves the command-line argument documentation.
    
    [hub]: https://github.com/github/hub

--- src/git.rs
+++ src/git.rs
@@ -16,27 +16,45 @@ pub struct GitConfig {
 }
 
 impl GitConfig {
-    pub fn new(git: String, branch: String) -> Result<Self, failure::Error> {
-        let remote = match Url::parse(&git) {
+    /// Creates a new `GitConfig` by parsing `git` as a URL or a local path.
+    pub fn new(git: &str, branch: String) -> Result<Self, failure::Error> {
+        let remote = match Url::parse(git) {
             Ok(u) => u,
             Err(ParseError::RelativeUrlWithoutBase) => {
-                let given_path = Path::new(&git);
+                let given_path = Path::new(git);
                 let mut git_path = PathBuf::new();
                 if given_path.is_relative() {
                     git_path.push(current_dir()?);
                     git_path.push(given_path);
+                    if !git_path.exists() {
+                        return Err(format_err!(
+                            "Failed parsing git remote {:?}: path {:?} doesn't exist",
+                            git,
+                            &git_path
+                        ));
+                    }
                 } else {
-                    git_path.push(&git)
+                    git_path.push(git)
                 }
                 let rel = "file://".to_string() + &git_path.to_str().unwrap_or("").to_string();
                 Url::parse(&rel)?
             }
-            Err(_) => return Err(format_err!("Failed parsing git remote: {}", &git)),
+            Err(err) => return Err(format_err!("Failed parsing git remote {:?}: {}", git, err)),
         };
 
         Ok(GitConfig {
             remote,
             branch: GitReference::Branch(branch),
+        })
+    }
+
+    /// Creates a new `GitConfig`, first with `new` and then as a GitHub `owner/repo` remote, like
+    /// [hub].
+    ///
+    /// [hub]: https://github.com/github/hub
+    pub fn new_abbr(git: &str, branch: String) -> Result<Self, failure::Error> {
+        Self::new(git, branch.clone()).or_else(|e| {
+            Self::new(&format!("https://github.com/{}.git", git), branch).map_err(|_| e)
         })
     }
 }
@@ -67,4 +85,64 @@ pub fn init(project_dir: &PathBuf, branch: &str) -> Result<GitRepository, failur
             .initial_head(branch),
     )
     .context("Couldn't init new repository")?)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn gitconfig_new_test() {
+        // Remote HTTPS URL.
+        let cfg = GitConfig::new(
+            "https://github.com/ashleygwilliams/cargo-generate.git",
+            "main".to_owned(),
+        )
+        .unwrap();
+
+        assert_eq!(
+            cfg.remote,
+            Url::parse("https://github.com/ashleygwilliams/cargo-generate.git").unwrap()
+        );
+        assert_eq!(cfg.branch, GitReference::Branch("main".to_owned()));
+
+        // Fails because "ashleygwilliams" is a "bad port number". Out of scope for now -- not sure
+        // how common SSH URLs are at this point anyways...?
+        assert!(GitConfig::new(
+            "ssh://git@github.com:ashleygwilliams/cargo-generate.git",
+            String::new(),
+        )
+        .is_err());
+
+        // Local path doesn't exist.
+        assert!(GitConfig::new("aslkdgjlaskjdglskj", String::new()).is_err());
+
+        // Local path does exist.
+        let remote = GitConfig::new("src", String::new())
+            .unwrap()
+            .remote
+            .into_string();
+        assert!(remote.ends_with("/src"));
+        assert!(remote.starts_with("file:///"));
+
+        // Absolute path.
+        // If this fails because you cloned this repository into a non-UTF-8 directory... all
+        // I can say is you probably had it comin'.
+        let remote = GitConfig::new(current_dir().unwrap().to_str().unwrap(), String::new())
+            .unwrap()
+            .remote
+            .into_string();
+        assert!(remote.starts_with("file:///"));
+    }
+
+    #[test]
+    fn gitconfig_new_abbr_test() {
+        // Abbreviated owner/repo form
+        assert_eq!(
+            GitConfig::new_abbr("ashleygwilliams/cargo-generate", String::new())
+                .unwrap()
+                .remote,
+            Url::parse("https://github.com/ashleygwilliams/cargo-generate.git").unwrap()
+        );
+    }
 }
