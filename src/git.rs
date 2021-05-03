5d63147786f6d61f4b96810b18d199844f09c35d
Cameron Dart <cdart@anduril.com>
2021-05-03 09:44:53 -0700

    feat(remote:ssh): support for ssh remote urls
     - support public ssh remote repository urls start with `git@` and `ssh://`
     - support private ssh remote repositories
     - add a public and an ignored private repo test (ignored because of ci/cd)
     - todo: documentation
     - use dir dependency to get home direction in a cross-platform manner
     - refactor git checkout, to use now close and repository builder
     - make clippy happy
     - based on @SkamDart's PR [#328](https://github.com/cargo-generate/cargo-generate/pull/328)
     - Resolves #325

--- src/git.rs
+++ src/git.rs
@@ -1,28 +1,35 @@
-use anyhow::{Context, Result};
+use anyhow::Context;
+use anyhow::Result;
 use cargo::core::GitReference;
-use cargo::sources::git::GitRemote;
-use cargo::util::config::Config;
-use git2::{
-    Cred, ProxyOptions, RemoteCallbacks, Repository as GitRepository, RepositoryInitOptions,
-};
+use git2::build::RepoBuilder;
+use git2::{Cred, FetchOptions, ProxyOptions, RemoteCallbacks, Repository, RepositoryInitOptions};
 use remove_dir_all::remove_dir_all;
-use std::env::{self, current_dir};
+use std::borrow::Cow;
+use std::env::current_dir;
 use std::path::{Path, PathBuf};
-use tempfile::Builder;
-use url::{ParseError, Url};
 
-pub(crate) struct GitConfig {
-    remote: Url,
-    branch: GitReference,
+#[derive(Debug, PartialEq)]
+enum RepoKind {
+    LocalFolder,
+    RemoteHttp,
+    RemoteHttps,
+    RemoteSsh,
+    Invalid,
 }
 
-impl GitConfig {
+pub(crate) struct GitConfig<'a> {
+    remote: Cow<'a, str>,
+    branch: GitReference,
+    kind: RepoKind,
+}
+
+impl<'a> GitConfig<'a> {
     /// Creates a new `GitConfig` by parsing `git` as a URL or a local path.
-    pub fn new(git: &str, branch: Option<String>) -> Result<Self> {
-        let remote = match Url::parse(git) {
-            Ok(u) => u,
-            Err(ParseError::RelativeUrlWithoutBase) => {
-                let given_path = Path::new(git);
+    pub fn new(git: Cow<'a, str>, branch: Option<String>) -> Result<Self> {
+        let (remote, kind) = match determine_repo_kind(git.as_ref()) {
+            RepoKind::Invalid => anyhow::bail!("Invalid git remote '{}'", &git),
+            RepoKind::LocalFolder => {
+                let given_path = Path::new(git.as_ref());
                 let mut git_path = PathBuf::new();
                 if given_path.is_relative() {
                     git_path.push(current_dir()?);
@@ -30,25 +37,24 @@ impl GitConfig {
                     if !git_path.exists() {
                         anyhow::bail!(
                             "Failed parsing git remote {:?}: path {:?} doesn't exist",
-                            git,
+                            &git,
                             &git_path
                         );
                     }
                 } else {
-                    git_path.push(git)
+                    git_path.push(git.as_ref());
                 }
-                Url::from_file_path(&git_path).map_err(|()| {
-                    anyhow::format_err!(
-                        "Failed parsing git remote (also tried as a file path): {}",
-                        &git
-                    )
-                })?
+                (
+                    format!("file://{}", git_path.display()).into(),
+                    RepoKind::LocalFolder,
+                )
             }
-            Err(err) => anyhow::bail!("Failed parsing git remote {:?}: {}", git, err),
+            k => (git, k),
         };
 
         Ok(GitConfig {
             remote,
+            kind,
             branch: branch
                 .map(GitReference::Branch)
                 .unwrap_or(GitReference::DefaultBranch),
@@ -59,140 +65,156 @@ impl GitConfig {
     /// [hub].
     ///
     /// [hub]: https://github.com/github/hub
-    pub fn new_abbr(git: &str, branch: Option<String>) -> Result<Self> {
-        Self::new(git, branch.clone()).or_else(|e| {
-            Self::new(&format!("https://github.com/{}.git", git), branch).map_err(|_| e)
+    pub fn new_abbr(git: Cow<'a, str>, branch: Option<String>) -> Result<Self> {
+        Self::new(git.clone(), branch.clone()).or_else(|_| {
+            let full_remote = format!("https://github.com/{}.git", &git);
+            Self::new(full_remote.into(), branch)
         })
     }
 }
 
-fn create_with_ssh(project_dir: &Path, args: GitConfig) -> Result<String> {
-    let mut callbacks = RemoteCallbacks::new();
+fn git_ssh_credentials_callback<'a>() -> Result<RemoteCallbacks<'a>> {
+    let mut priv_key = dirs::home_dir().context("$HOME was not set")?;
+    priv_key.push(".ssh/id_rsa");
+    let mut cb = RemoteCallbacks::new();
+    cb.credentials(
+        move |_url, username_from_url: Option<&str>, _allowed_types| {
+            Cred::ssh_key(username_from_url.unwrap_or("git"), None, &priv_key, None)
+        },
+    );
+    Ok(cb)
+}
 
-    callbacks.credentials(|_url, username_from_url, _allowed_types| {
-        Cred::ssh_key(
-            username_from_url.unwrap_or("git"),
-            None,
-            Path::new(&format!("{}/.ssh/id_rsa", env::var("HOME").unwrap())),
-            None,
-        )
-    });
+/// thanks to @extrawurst for pointing this out
+/// https://github.com/extrawurst/gitui/blob/master/asyncgit/src/sync/branch/mod.rs#L38
+fn get_branch_name_repo(repo: &Repository) -> Result<String> {
+    let iter = repo.branches(None)?;
 
-    let branch = match args.branch {
-        GitReference::Branch(branch_name) => branch_name,
-        GitReference::DefaultBranch => "master".into(),
-        _ => unreachable!(),
-    };
+    for b in iter {
+        let b = b?;
 
-    let mut fo = git2::FetchOptions::new();
-    fo.remote_callbacks(callbacks);
+        if b.0.is_head() {
+            let name = b.0.name()?.unwrap_or("");
+            return Ok(name.into());
+        }
+    }
 
-    let mut builder = git2::build::RepoBuilder::new();
+    anyhow::bail!("A repo has no Head")
+}
+
+fn init_all_submodules(repo: &Repository) -> Result<()> {
+    for mut sub in repo.submodules().unwrap() {
+        sub.update(true, None)?;
+    }
+
+    Ok(())
+}
+
+pub(crate) fn create(project_dir: &Path, args: GitConfig) -> Result<String> {
+    let mut builder = RepoBuilder::new();
+    if let GitReference::Branch(branch_name) = &args.branch {
+        builder.branch(branch_name.as_str());
+    }
+
+    let mut fo = FetchOptions::new();
+    match args.kind {
+        RepoKind::LocalFolder => {}
+        RepoKind::RemoteHttp | RepoKind::RemoteHttps => {
+            let mut proxy = ProxyOptions::new();
+            proxy.auto();
+            fo.proxy_options(proxy);
+        }
+        RepoKind::RemoteSsh => {
+            let callbacks = git_ssh_credentials_callback()?;
+            fo.remote_callbacks(callbacks);
+        }
+        RepoKind::Invalid => {
+            unreachable!()
+        }
+    }
     builder.fetch_options(fo);
-    builder.branch(&branch);
 
-    builder.clone(args.remote.as_str(), project_dir)?;
+    let repo = builder.clone(args.remote.as_ref(), project_dir)?;
+    let branch = get_branch_name_repo(&repo)?;
+    init_all_submodules(&repo)?;
+    remove_history(project_dir)?;
 
     Ok(branch)
 }
 
-fn create_with_http(project_dir: &Path, args: GitConfig) -> Result<String> {
-    let temp = Builder::new().prefix(project_dir).tempdir()?;
-    let config = Config::default()?;
-    let remote = GitRemote::new(&args.remote);
-
-    let ((db, rev), branch_name) = match &args.branch {
-        GitReference::Branch(branch_name) => (
-            remote.checkout(&temp.path(), None, &args.branch, None, &config)?,
-            branch_name.clone(),
-        ),
-        GitReference::DefaultBranch => {
-            // Cargo has a specific behavior for now for handling the "default" branch. It forces
-            // it to the branch named "master" even if the actual default branch of the repository
-            // is something else. They intent to change this behavior in the future but they don't
-            // want to break the compatibility.
-            //
-            // See issues:
-            //  - https://github.com/rust-lang/cargo/issues/8364
-            //  - https://github.com/rust-lang/cargo/issues/8468
-            let repo = git2::Repository::init(&temp.path())?;
-            let mut origin = repo.remote_anonymous(remote.url().as_str())?;
-            let mut proxy = ProxyOptions::new();
-            proxy.auto();
-            origin.connect_auth(git2::Direction::Fetch, None, Some(proxy))?;
-            let default_branch = origin.default_branch()?;
-            let branch_name = default_branch
-                .as_str()
-                .unwrap_or("refs/heads/master")
-                .replace("refs/heads/", "");
-            (
-                remote.checkout(
-                    &temp.path(),
-                    None,
-                    &GitReference::Branch(branch_name.clone()),
-                    None,
-                    &config,
-                )?,
-                branch_name,
-            )
-        }
-        _ => unreachable!(),
-    };
-
-    // This clones the remote and handles all the submodules
-    db.copy_to(rev, project_dir, &config)?;
-    Ok(branch_name)
-}
-
-pub(crate) fn create(project_dir: &Path, args: GitConfig) -> Result<String> {
-    if args.remote.to_string().contains("ssh://") {
-        create_with_ssh(project_dir, args)
-    } else {
-        create_with_http(project_dir, args)
-    }
-}
-
-pub(crate) fn remove_history(project_dir: &Path) -> Result<()> {
+fn remove_history(project_dir: &Path) -> Result<()> {
     remove_dir_all(project_dir.join(".git")).context("Error cleaning up cloned template")?;
     Ok(())
 }
 
-pub fn init(project_dir: &Path, branch: &str) -> Result<GitRepository> {
-    GitRepository::discover(project_dir).or_else(|_| {
+pub fn init(project_dir: &Path, branch: &str) -> Result<Repository> {
+    Repository::discover(project_dir).or_else(|_| {
         let mut opts = RepositoryInitOptions::new();
         opts.bare(false);
         opts.initial_head(branch);
-        GitRepository::init_opts(project_dir, &opts).context("Couldn't init new repository")
+        Repository::init_opts(project_dir, &opts).context("Couldn't init new repository")
     })
+}
+
+/// determines what kind of repository we got
+fn determine_repo_kind(remote_url: &str) -> RepoKind {
+    if remote_url.starts_with("ssh://") || remote_url.starts_with("git@") {
+        RepoKind::RemoteSsh
+    } else if remote_url.starts_with("http://") {
+        RepoKind::RemoteHttp
+    } else if remote_url.starts_with("https://") {
+        RepoKind::RemoteHttps
+    } else if Path::new(remote_url).exists() {
+        RepoKind::LocalFolder
+    } else {
+        RepoKind::Invalid
+    }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+    use url::Url;
 
     const REPO_URL: &str = "https://github.com/cargo-generate/cargo-generate.git";
+    const REPO_URL_SSH: &str = "git@github.com:cargo-generate/cargo-generate.git";
 
     #[test]
-    #[should_panic(expected = "invalid port number")]
-    fn should_fail_for_ssh_remote_urls() {
-        GitConfig::new(
-            REPO_URL
-                .replace("https://github.com/", "ssh://git@github.com:")
-                .as_str(),
-            None,
-        )
-        .unwrap();
+    fn should_determine_repo_kind() {
+        for (u, k) in &[
+            (REPO_URL, RepoKind::RemoteHttps),
+            (
+                "http://github.com/cargo-generate/cargo-generate.git",
+                RepoKind::RemoteHttp,
+            ),
+            (REPO_URL_SSH, RepoKind::RemoteSsh),
+            (
+                "ssh://git@github.com:cargo-generate/cargo-generate.git",
+                RepoKind::RemoteSsh,
+            ),
+            ("./", RepoKind::LocalFolder),
+            ("ftp://foobar.bak", RepoKind::Invalid),
+        ] {
+            let kind = determine_repo_kind(u);
+            assert_eq!(&kind, k, "{} is not a {:?}", u, k);
+        }
     }
 
     #[test]
-    #[should_panic(expected = "aslkdgjlaskjdglskj\" doesn't exist")]
+    fn should_not_fail_for_ssh_remote_urls() {
+        let config = GitConfig::new(REPO_URL_SSH.into(), None).unwrap();
+        assert_eq!(config.kind, RepoKind::RemoteSsh);
+    }
+
+    #[test]
+    #[should_panic(expected = "Invalid git remote 'aslkdgjlaskjdglskj'")]
     fn should_fail_for_non_existing_local_path() {
-        GitConfig::new("aslkdgjlaskjdglskj", None).unwrap();
+        GitConfig::new("aslkdgjlaskjdglskj".into(), None).unwrap();
     }
 
     #[test]
     fn should_support_a_local_relative_path() {
-        let remote: String = GitConfig::new("src", None).unwrap().remote.into();
+        let remote: String = GitConfig::new("src".into(), None).unwrap().remote.into();
         assert!(
             remote.ends_with("/src"),
             "remote {} ends with /src",
@@ -213,10 +235,11 @@ mod tests {
         // Absolute path.
         // If this fails because you cloned this repository into a non-UTF-8 directory... all
         // I can say is you probably had it comin'.
-        let remote: String = GitConfig::new(current_dir().unwrap().to_str().unwrap(), None)
-            .unwrap()
-            .remote
-            .into();
+        let remote: String =
+            GitConfig::new(current_dir().unwrap().display().to_string().into(), None)
+                .unwrap()
+                .remote
+                .into();
         assert!(
             remote.starts_with("file:///"),
             "remote {} starts with file:///",
@@ -227,19 +250,20 @@ mod tests {
     #[test]
     fn should_test_happy_path() {
         // Remote HTTPS URL.
-        let cfg = GitConfig::new(REPO_URL, Some("main".to_owned())).unwrap();
+        let cfg = GitConfig::new(REPO_URL.into(), Some("main".to_owned())).unwrap();
 
-        assert_eq!(cfg.remote, Url::parse(REPO_URL).unwrap());
+        assert_eq!(cfg.remote.as_ref(), Url::parse(REPO_URL).unwrap().as_str());
         assert_eq!(cfg.branch, GitReference::Branch("main".to_owned()));
     }
 
     #[test]
     fn should_support_abbreviated_repository_short_urls_like() {
         assert_eq!(
-            GitConfig::new_abbr("cargo-generate/cargo-generate", None)
+            GitConfig::new_abbr("cargo-generate/cargo-generate".into(), None)
                 .unwrap()
-                .remote,
-            Url::parse(REPO_URL).unwrap()
+                .remote
+                .as_ref(),
+            Url::parse(REPO_URL).unwrap().as_str()
         );
     }
 }
