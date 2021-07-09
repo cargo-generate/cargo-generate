070fbdfd6ca4268dafdc36db7cbda3e596267347
Sven Assmann <sven.assmann.it@gmail.com>
2021-07-09 10:38:54 +0200

    feat(ssh-remote): support for ssh private key files

--- src/emoji.rs
+++ src/emoji.rs
@@ -5,3 +5,4 @@ pub(crate) static SPARKLE: Emoji<'_, '_> = Emoji("✨  ", "");
 pub(crate) static WARN: Emoji<'_, '_> = Emoji("⚠️  ", "");
 pub(crate) static WRENCH: Emoji<'_, '_> = Emoji("🔧  ", "");
 pub(crate) static SHRUG: Emoji<'_, '_> = Emoji("🤷  ", "");
+pub(crate) static INFO: Emoji<'_, '_> = Emoji("💡  ", "");
