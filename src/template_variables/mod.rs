0681696a25a854d2f2b52a3de89c3e9b8fdaa283
Johnny T. Vestergaard <jtv@jrtv.dk>
2021-08-07 20:11:07 +0200

    add testcases around regex for definitions

--- src/template_variables/mod.rs
+++ src/template_variables/mod.rs
@@ -8,7 +8,7 @@ use crate::{config::ConfigValues, emoji, Args};
 use anyhow::Result;
 use console::style;
 use regex::Regex;
-use std::{collections::HashMap, fs, path::Path};
+use std::{collections::HashMap, fmt::Display, fs, path::Path};
 use toml::Value;
 
 pub(crate) use authors::{get_authors, Authors};
@@ -25,7 +25,7 @@ pub(crate) fn resolve_template_values(args: &Args) -> Result<HashMap<String, Val
             read_template_values_file(path)
         })?;
 
-    add_cli_defined_values(&mut template_values, args)?;
+    add_cli_defined_values(&mut template_values, &args.define)?;
 
     Ok(template_values)
 }
@@ -44,27 +44,113 @@ fn read_template_values_file(path: &Path) -> Result<HashMap<String, Value>> {
     }
 }
 
-fn add_cli_defined_values(template_values: &mut HashMap<String, Value>, args: &Args) -> Result<()> {
-    let regex = Regex::new(r"(\w+)=(.+)").unwrap();
+fn add_cli_defined_values<S: AsRef<str> + Display>(
+    template_values: &mut HashMap<String, Value>,
+    definitions: &[S],
+) -> Result<()> {
+    let key_value_regex = Regex::new(r"^([a-zA-Z]+[a-zA-Z0-9\-_]*)\s*=\s*(.+)$").unwrap();
 
-    args.define
+    definitions
         .iter()
-        .try_fold(template_values, |template_values, d| {
-            match regex.captures(d) {
+        .try_fold(
+            template_values,
+            |template_values, definition| match key_value_regex.captures(definition.as_ref()) {
                 Some(cap) => {
-                    let k = cap.get(1).unwrap().as_str().to_string();
-                    let v = cap.get(2).unwrap().as_str().to_string();
-                    println!("{} => '{}'", k, v);
-                    template_values.insert(k, Value::from(v));
+                    let key = cap.get(1).unwrap().as_str().to_string();
+                    let value = cap.get(2).unwrap().as_str().to_string();
+                    println!("{} => '{}'", key, value);
+                    template_values.insert(key, Value::from(value));
                     Ok(template_values)
                 }
                 None => Err(anyhow::anyhow!(
                     "{} {} {}",
                     emoji::ERROR,
                     style("Failed to parse value:").bold().red(),
-                    style(d).bold().red(),
+                    style(definition).bold().red(),
                 )),
-            }
-        })?;
+            },
+        )?;
     Ok(())
+}
+
+#[cfg(test)]
+mod test {
+    use super::add_cli_defined_values;
+    use std::collections::HashMap;
+
+    #[test]
+    fn names_must_start_with_word_char() {
+        let mut template_values = HashMap::new();
+        let definitions = vec!["0key=42"];
+        let result = add_cli_defined_values(&mut template_values, &definitions);
+        assert!(result.is_err());
+
+        let definitions = vec!["$key=42"];
+        let result = add_cli_defined_values(&mut template_values, &definitions);
+        assert!(result.is_err());
+
+        let definitions = vec!["-key=42"];
+        let result = add_cli_defined_values(&mut template_values, &definitions);
+        assert!(result.is_err());
+
+        let definitions = vec!["_key=42"];
+        let result = add_cli_defined_values(&mut template_values, &definitions);
+        assert!(result.is_err());
+    }
+
+    #[test]
+    fn names_may_contain_digits() {
+        let mut template_values = HashMap::new();
+        let definitions = vec!["my0123456789key=42"];
+        let result = add_cli_defined_values(&mut template_values, &definitions);
+        assert!(result.is_ok());
+
+        let val = template_values
+            .get("my0123456789key")
+            .unwrap()
+            .as_str()
+            .unwrap();
+        assert_eq!(val, "42");
+    }
+
+    #[test]
+    fn names_may_contain_dash() {
+        let mut template_values = HashMap::new();
+        let definitions = vec!["my-key=42"];
+        let result = add_cli_defined_values(&mut template_values, &definitions);
+        assert!(result.is_ok());
+
+        let val = template_values.get("my-key").unwrap().as_str().unwrap();
+        assert_eq!(val, "42");
+    }
+
+    #[test]
+    fn names_may_contain_underscore() {
+        let mut template_values = HashMap::new();
+        let definitions = vec!["my_key=42"];
+        let result = add_cli_defined_values(&mut template_values, &definitions);
+        assert!(result.is_ok());
+
+        let val = template_values.get("my_key").unwrap().as_str().unwrap();
+        assert_eq!(val, "42");
+    }
+
+    #[test]
+    fn spaces_are_not_allowed_in_names() {
+        let mut template_values = HashMap::new();
+        let definitions = vec!["my key=42"];
+        let result = add_cli_defined_values(&mut template_values, &definitions);
+        assert!(result.is_err());
+    }
+
+    #[test]
+    fn spaces_around_assignment_is_ok() {
+        let mut template_values = HashMap::new();
+        let definitions = vec!["key   =      42"];
+        let result = add_cli_defined_values(&mut template_values, &definitions);
+        assert!(result.is_ok());
+
+        let val = template_values.get("key").unwrap().as_str().unwrap();
+        assert_eq!(val, "42");
+    }
 }
