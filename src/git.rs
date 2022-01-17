0c5301c7f831a27167cd5d523ef7e5a9338e9603
Sven Assmann <sven.assmann.it@gmail.com>
2022-01-17 02:22:44 +0100

    fix(abbrev): fix index out of range for git abbrev

--- src/git.rs
+++ src/git.rs
@@ -88,11 +88,15 @@ impl<'a> GitConfig<'a> {
 
 fn abbreviated_git_url_to_full_remote(git: impl AsRef<str>) -> String {
     let git = git.as_ref();
-    match &git[..3] {
-        "gl:" => format!("https://gitlab.com/{}.git", &git[3..]),
-        "bb:" => format!("https://bitbucket.org/{}.git", &git[3..]),
-        "gh:" => format!("https://github.com/{}.git", &git[3..]),
-        _ => git.to_owned(),
+    if git.len() >= 3 {
+        match &git[..3] {
+            "gl:" => format!("https://gitlab.com/{}.git", &git[3..]),
+            "bb:" => format!("https://bitbucket.org/{}.git", &git[3..]),
+            "gh:" => format!("https://github.com/{}.git", &git[3..]),
+            _ => git.to_owned(),
+        }
+    } else {
+        git.to_owned()
     }
 }
 
