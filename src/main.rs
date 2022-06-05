7aedd5a2ba4fe2e6d05f50a52caddff71a615b9f
yozhgoor <yozhgoor@outlook.com>
2022-06-06 00:00:28 +0200

    Replace `structopt` by `clap`

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,14 @@
 use anyhow::Result;
-use cargo_generate::{generate, Cli};
-use structopt::StructOpt;
+use cargo_generate::{generate, Args};
+use clap::Parser;
+use std::env;
 
 fn main() -> Result<()> {
-    let Cli::Generate(args) = Cli::from_args();
+    let mut args = env::args().peekable();
+    let command = args.next();
+    args.next_if(|x| x.as_str() == "generate");
+
+    let args = Args::parse_from(command.into_iter().chain(args));
     generate(args)?;
 
     Ok(())
