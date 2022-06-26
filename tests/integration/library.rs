ef4bc4b3e22697f637c5ef90a7889e293e43f420
yozhgoor <yozhgoor@outlook.com>
2022-06-26 11:03:36 +0200

    Reduce to one integration test

--- tests/integration/library.rs
+++ tests/integration/library.rs
@@ -2,7 +2,7 @@ use crate::helpers::project_builder::tmp_dir;
 use cargo_generate::{generate, GenerateArgs, TemplatePath, Vcs};
 
 #[test]
-fn it_allows_generate_call_with_public_args() {
+fn it_allows_generate_call_with_public_args_and_return_generated_path() {
     let template = tmp_dir()
         .file(
             "Cargo.toml",
@@ -46,61 +46,10 @@ version = "0.1.0"
 
     // need to cd to the dir as we aren't running in the cargo shell.
     assert!(std::env::set_current_dir(&dir.root).is_ok());
-    assert!(generate(args_exposed).is_ok());
-
-    assert!(dir
-        .read("foobar_project/Cargo.toml")
-        .contains("foobar_project"));
-}
-
-#[test]
-fn it_returns_the_path_of_the_generated_project() {
-    let template = tmp_dir()
-        .file(
-            "Cargo.toml",
-            r#"[package]
-name = "{{project-name}}"
-description = "A wonderful project"
-version = "0.1.0"
-"#,
-        )
-        .init_git()
-        .build();
-
-    let dir = tmp_dir().build();
-
-    let args_exposed: GenerateArgs = GenerateArgs {
-        template_path: TemplatePath {
-            auto_path: None,
-            git: Some(format!("{}", template.path().display())),
-            branch: Some(String::from("main")),
-            path: None,
-            favorite: None,
-            subfolder: None,
-        },
-        name: Some(String::from("foobar_project")),
-        force: true,
-        vcs: Vcs::Git,
-        verbose: true,
-        template_values_file: None,
-        silent: false,
-        list_favorites: false,
-        config: None,
-        bin: true,
-        lib: false,
-        ssh_identity: None,
-        define: vec![],
-        init: false,
-        destination: None,
-        force_git_init: false,
-        allow_commands: false,
-    };
-
-    // need to cd to the dir as we aren't running in the cargo shell.
-    assert!(std::env::set_current_dir(&dir.root).is_ok());
-
-    let generated_path = generate(args_exposed).expect("cannot generate project");
-    assert_eq!(generated_path, dir.path().join("foobar_project"));
+    assert_eq!(
+        generate(args_exposed).expect("cannot generate project"),
+        dir.path().join("foobar_project")
+    );
 
     assert!(dir
         .read("foobar_project/Cargo.toml")
