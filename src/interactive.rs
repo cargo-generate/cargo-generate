ac45a2158d6b81f92c83d575830b3b55492c65c1
Johnny T. Vestergaard <jtv@jrtv.dk>
2022-07-03 17:53:29 +0200

    fix lint error

--- src/interactive.rs
+++ src/interactive.rs
@@ -98,7 +98,7 @@ pub fn prompt_for_variable(variable: &TemplateSlots) -> Result<String> {
     }
 }
 
-pub(super) fn variable(variable: &TemplateSlots, provided_value: Option<&str>) -> Result<Value> {
+pub fn variable(variable: &TemplateSlots, provided_value: Option<&str>) -> Result<Value> {
     let user_input = provided_value
         .map(|v| Ok(v.to_string()))
         .unwrap_or_else(|| prompt_for_variable(variable))?;
