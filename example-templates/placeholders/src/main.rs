3ca71eab1e6d230726fffd8aa9dd3cc0fdfc41f3
Sam Vente <savente93@proton.me>
2025-01-24 12:50:08 +0100

    feat(example): add multi-choice to placeholder example

--- example-templates/placeholders/src/main.rs
+++ example-templates/placeholders/src/main.rs
@@ -2,4 +2,5 @@ fn main() {
     println!("Hello, {{authors}}!");
     println!("The boolean was: {{some_bool}}");
     println!("Your choice was: {{choice}}");
+    println!("Your multi-choice was: {{multi-choice}}");
 }
