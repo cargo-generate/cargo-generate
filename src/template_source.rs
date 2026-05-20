e4432dc96b13e0c00fb761c584c052d2cf472c9b
Sven Kanoldt <sven@d34dl0ck.me>
2026-05-20 23:24:26 +0200

    feat: add GitHost::to_url to render provider URL forms
    Signed-off-by: Sven Kanoldt <sven@d34dl0ck.me>

--- src/template_source.rs
+++ src/template_source.rs
@@ -23,3 +23,51 @@ pub enum TemplateSource {
     LocalAbsolute(PathBuf),
     Favorite(Box<TemplateSource>),
 }
+
+impl GitHost {
+    pub fn to_url(&self, owner_repo: &str) -> String {
+        match self {
+            Self::GitHub => format!("https://github.com/{owner_repo}.git"),
+            Self::GitLab => format!("https://gitlab.com/{owner_repo}.git"),
+            Self::Bitbucket => format!("https://bitbucket.org/{owner_repo}.git"),
+            Self::SourceHut => format!("https://git.sr.ht/~{owner_repo}"),
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn git_host_to_url_github() {
+        assert_eq!(
+            GitHost::GitHub.to_url("owner/repo"),
+            "https://github.com/owner/repo.git"
+        );
+    }
+
+    #[test]
+    fn git_host_to_url_gitlab() {
+        assert_eq!(
+            GitHost::GitLab.to_url("owner/repo"),
+            "https://gitlab.com/owner/repo.git"
+        );
+    }
+
+    #[test]
+    fn git_host_to_url_bitbucket() {
+        assert_eq!(
+            GitHost::Bitbucket.to_url("owner/repo"),
+            "https://bitbucket.org/owner/repo.git"
+        );
+    }
+
+    #[test]
+    fn git_host_to_url_sourcehut_inserts_tilde() {
+        assert_eq!(
+            GitHost::SourceHut.to_url("user/repo"),
+            "https://git.sr.ht/~user/repo"
+        );
+    }
+}
